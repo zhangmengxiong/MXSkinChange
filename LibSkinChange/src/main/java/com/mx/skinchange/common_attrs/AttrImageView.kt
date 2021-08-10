4dfcdc95afc0a36a7765173551c166d629a2b029
zhangmengxiong <zmx_final@163.com>
2021-08-10 11:25:44 +0800

    666

--- LibSkinChange/src/main/java/com/mx/skinchange/common_attrs/AttrImageView.kt
+++ LibSkinChange/src/main/java/com/mx/skinchange/common_attrs/AttrImageView.kt
@@ -2,12 +2,14 @@ package com.mx.skinchange.common_attrs
 
 import android.content.res.TypedArray
 import android.util.AttributeSet
+import androidx.core.widget.ImageViewCompat
 import com.mx.skinchange.R
 import com.mx.skinchange.common_views.CommonImageView
 import com.mx.skinchange.factory.SkinResourceLoader
 
 open class AttrImageView(val view: CommonImageView) : AttrBase {
     private var srcResId = AttrBase.INVALID_ID
+    private var srcTintResId = AttrBase.INVALID_ID
 
     override fun initAttrs(attrs: AttributeSet?, defStyleAttr: Int) {
         val a: TypedArray = view.context.obtainStyledAttributes(
@@ -17,10 +19,13 @@ open class AttrImageView(val view: CommonImageView) : AttrBase {
         )
         try {
             srcResId = getResourceId(a, R.styleable.AttrImageView_android_src)
+            srcTintResId = getResourceId(a, R.styleable.AttrImageView_android_tint)
         } finally {
             a.recycle()
         }
+
         applyAttrs()
+        applyTintRes() // 初始化运行一次
     }
 
     override fun applyAttrs() {
@@ -30,6 +35,15 @@ open class AttrImageView(val view: CommonImageView) : AttrBase {
         }
         val drawable = SkinResourceLoader.loadDrawable(view.context, resId)
         view.setImageDrawable(drawable)
+    }
+
+    private fun applyTintRes() {
+        val resId = checkResourceId(srcTintResId)
+        if (resId == AttrBase.INVALID_ID) {
+            return
+        }
+        val colorStateList = SkinResourceLoader.loadColorStateList(view.context, resId)
+        ImageViewCompat.setImageTintList(view, colorStateList)
     }
 
     fun setImageResource(res: Int) {
